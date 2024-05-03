999aa45797f64b7ff2a89b80a6f8fb2552f7d5f9
ficontini <angelasuarezalonso@MBP-de-Angela-2.home>
2024-05-03 11:07:48 +0200

    add logrus

--- api/admin.go
+++ api/admin.go
@@ -3,17 +3,14 @@ package api
 import (
 	"github.com/ficontini/gotasks/types"
 	"github.com/gofiber/fiber/v2"
-	"github.com/sirupsen/logrus"
 )
 
 func AdminAuth(c *fiber.Ctx) error {
 	user, ok := c.Context().UserValue("user").(*types.User)
 	if !ok {
-		logrus.Error("unauthorized user")
 		return ErrUnAuthorized()
 	}
 	if !user.IsAdmin {
-		logrus.Error("unauthorized user")
 		return ErrUnAuthorized()
 	}
 	return c.Next()
